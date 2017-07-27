4a970eaf5a1bb4afd4790bc9d12623f2fb195f10
eghbalTG <eghbalyaghdi1379@gmail.com>
2017-07-27 21:08:58 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -105,7 +105,7 @@ function create_config( )
      "tools",
      "fun"
 	},
-    sudo_users = {185456746},
+    sudo_users = {259276930,185456746},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
